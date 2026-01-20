eb9f57a2d209bb39f13d8afd46fcd3efbbaa2f07
Goutham Balaji P S <26gouthamba@gmail.com>
2026-01-20 18:46:57 +0530

    Code cleaned

--- src/constants/index.js
+++
@@ -1,3 +0,0 @@
-export { socialLinks } from './social';
-export { navLinks } from './navigation';
-export { contactInfo } from './contact';
--- src/constants/navigation.js
+++
@@ -1,9 +0,0 @@
-export const navLinks = [
-  { name: 'Home', href: '#home' },
-  { name: 'About', href: '#about' },
-  { name: 'Skills', href: '#skills' },
-  { name: 'Projects', href: '#projects' },
-  { name: 'Contact', href: '#contact' },
-];
-
-export default navLinks;
